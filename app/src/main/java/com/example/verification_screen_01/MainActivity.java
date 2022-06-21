df85d158a8aee8d180eec5791eaedb129f9f80c4
Malik Muhammad Awais <181314@pakaims.edu.pk>
2022-06-21 13:44:34 +0500

    Initial commit

---
+++ app/src/main/java/com/example/verification_screen_01/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.verification_screen_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.hbb20.CountryCodePicker;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
