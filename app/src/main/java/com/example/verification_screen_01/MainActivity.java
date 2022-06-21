8961ae44c234c5d1a3d8f85a10f8121d604ab772
Malik Muhammad Awais <181314@pakaims.edu.pk>
2022-06-21 17:25:55 +0500

    initial commit 2

--- app/src/main/java/com/example/verification_screen_01/MainActivity.java
+++ app/src/main/java/com/example/verification_screen_01/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.hbb20.CountryCodePicker;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,8 +23,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sendbtn = (Button) findViewById(R.id.btnSend);
+        sendbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity2();
+            }
+        });
+
         initializeViews();
-        listeners();
+//        listeners();
+    }
+    public void openActivity2(){
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
     }
 
     private void initializeViews(){
@@ -31,24 +44,24 @@ public class MainActivity extends AppCompatActivity {
         phonetextview = (EditText) findViewById(R.id.editTextPhone);
         sendbtn = (Button) findViewById(R.id.btnSend);
     }
-    private  void listeners(){
-        sendbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //get Variables
-                String code = ccp.getSelectedCountryCode();
-                String country = ccp.getSelectedCountryEnglishName();
-                String number = ccp.getTextView_selectedCountry().getText().toString();
-
-
-                //Create Toast  String number = ccp.phonetextview.getText().toString();
-                Context context = getApplicationContext();
-                CharSequence text = "Country : "+ country + ", Value Sent: "+ code+" " + number;
-                int duration = Toast.LENGTH_SHORT;
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
-            }
-        });
-    }
+//    private  void listeners(){
+//        sendbtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                //get Variables
+//                String code = ccp.getSelectedCountryCode();
+//                String country = ccp.getSelectedCountryEnglishName();
+//                String number = ccp.getTextView_selectedCountry().getText().toString();
+//
+//
+//                //Create Toast  String number = ccp.phonetextview.getText().toString();
+//                Context context = getApplicationContext();
+//                CharSequence text = "Country : "+ country + ", Value Sent: "+ code+" " + number;
+//                int duration = Toast.LENGTH_SHORT;
+//                Toast toast = Toast.makeText(context, text, duration);
+//                toast.show();
+//            }
+//        });
+//    }
 
 }
---
+++ app/src/main/java/com/example/verification_screen_01/MainActivity2.java
@@ -0,0 +1,35 @@
+package com.example.verification_screen_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private TextView editNumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        editNumber = (TextView) findViewById(R.id.editNumber);
+        editNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity();
+            }
+        });
+
+    }
+    public void openActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
