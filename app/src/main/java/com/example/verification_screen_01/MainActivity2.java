df147979cdad34bcd2672b5af877c933ce1ddd9d
Malik Muhammad Awais <181314@pakaims.edu.pk>
2022-06-22 09:51:53 +0500

    initial commit 3

--- app/src/main/java/com/example/verification_screen_01/MainActivity2.java
+++ app/src/main/java/com/example/verification_screen_01/MainActivity2.java
@@ -6,11 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 public class MainActivity2 extends AppCompatActivity {
+
+
 
     private TextView editNumber;
 
